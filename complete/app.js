25085de2b4c88cb1ed425f0c751024a257769a00
Steve Moore <srmoore@gmail.com>
2017-09-19 20:04:28 -0400

    Added Passport and Express-Session and initialized them.

--- complete/app.js
+++ complete/app.js
@@ -10,6 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+// passport and sessions are required, so add them here.
+// You'll have to 'npm install' these.
+const passport = require('passport');
+const session = require('express-session');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -21,6 +26,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Here we'll set up using server side sessions
+// this is using an in memory system. Use something like memcached or redis in production
+app.use(session({ secret: 'ourapplicationsecret', // change this
+		  resave: false,
+		  saveUninitialized: false}));
+
+// initialize passport here
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport has to serialize/deserialize users to the sessions.
+//  This is a simple case
+passport.serializeUser((user, done) => { done(null, user);});
+passport.deserializeUser((user, done) => { done( null, user);});
+
 
 app.use('/', index);
 app.use('/users', users);
