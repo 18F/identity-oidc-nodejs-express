0c2c16b2bb727b769ef5cc7456bbaffaf4b158ac
Steve Moore <srmoore@gmail.com>
2017-09-19 20:56:08 -0400

    Added in oidc-client, and configuration for it TODO: Actually set up some 'authenticated' paths and the callback URL

--- complete/app.js
+++ complete/app.js
@@ -15,6 +15,32 @@ var app = express();
 const passport = require('passport');
 const session = require('express-session');
 
+// requirements for openid-client, as well as reading in the key we generated
+const fs = require('fs'); // used to read in the key file
+const jose = require('node-jose'); // used to parse the keystore
+const Issue = require('openid-client').Issuer; 
+const Strategy = require('openid-client').Strategy;
+
+// filename for the keys
+const jwk_file = "./full_key.jwk";
+
+// OpenID Connect provider url for discovery
+const oidc_discover_url = "https://mitreid.org";
+
+// Params for creating the oidc client
+const client_params = {
+	client_id: 'login-nodejs-govt-test',
+	token_endpoint_auth_method: 'private_key_jwt'
+};
+
+// Params for the OIDC Passport Strategy
+const strat_params = {
+	redirect_uri: 'http://localhost:3000/openid-connect-login',
+	scope: 'openid profile email phone address',
+	response: ['userinfo']
+};
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -41,6 +67,41 @@ app.use(passport.session());
 //  This is a simple case
 passport.serializeUser((user, done) => { done(null, user);});
 passport.deserializeUser((user, done) => { done( null, user);});
+
+// load keystore and set up openid-client
+const jwk_json = JSON.parse(fs.readFileSync(jwk_file, "utf-8"));
+// load the keystore. returns a Promise
+let load_keystore = jose.JWK.asKeyStore(jwk_json);
+// discover the Issuer. returns a Promise
+//   you can also set this up manually, see the project documentation
+let discover_issuer = Issuer.discover(oidc_discover_url);
+
+// Create a client, and use it set up a Strategy for passport to use
+// since we need both the Issuer and the keystore, we'll use Promise.all()
+Promise.all([load_keystore, discover_issuer])
+	.then([ks, myIssuer] => {
+		console.log("Found Issuer: ", myIssuer);
+		const oidc_client = new myIssuer.Client(client_params, ks);
+		console.log("Created client: ", oidc_client);
+		
+		// create a strategy along with the function that processes the results
+		passport.use('oidc', new Strategy({client: oidc_client, params: strat_params}, (tokenset, userinfo, done) => {
+			// we're just loging out the tokens. Don't do this in production
+			console.log('tokenset', tokenset);
+			console.log('access_token', tokenset.access_token);
+			console.log('id_token', tokenset.id_token);
+			console.log('claims', tokenset.claims);
+			console.log('userinfo', userinfo);
+			
+			// to do anything, we need to return a user. 
+			// if you are storing information in your application this would use some middlewhere and a database
+			// the call would typically look like
+			// User.findOrCreate(userinfo.sub, userinfo, (err, user) => { done(err, user); });
+			// we'll just pass along the userinfo object as a simple 'user' object
+			return done(null, userinfo);
+		}));
+	}) // close off the .then()
+	.catch((err) => {console.log("Error in OIDC setup", err);});
 
 
 app.use('/', index);
